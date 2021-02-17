0d9eba3379263b2e3bc9200c8207a71ae73a36e3
Ibmug <rosv58@gmail.com>
2021-02-16 21:59:50 -0600

    Uploading to Heroku

--- server.js
+++ server.js
@@ -10,10 +10,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASS + "@cluster0.om7ei.mongodb.net/fitnessTrack?retryWrites=true&w=majority";
+
+try{
+  mongoose.connect(process.env.MONGODB_URI || uri);
+
+}catch(error){
+  throw(error);
+}
+
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
